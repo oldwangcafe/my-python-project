696e7b29ed0a5a621171670f88e26df449d9100a
oldwangcafe <oldwangcafe@gmail.com>
2024-10-09 19:12:16 +0800

    Add existing Python project

---
+++ python01.py
@@ -0,0 +1,14 @@
+temp = 'Hello world'
+print(temp)
+
+
+#https://github.com/oldwangcafe/my-python-project.git
+#git remote add origin https://github.com/oldwangcafe/my-python-project.git
+#git add .
+#git commit -m "Initial commit"
+#git branch -M main
+#git push -u origin main
+#git clone https://github.com/oldwangcafe/my-python-project.git
+
+
+
